769ca539b26e45c61bd3b826d154e431a247b31e
sanjaysreekumar1000-bit <sanjaysreekumar1000@gmail.com>
2025-12-25 14:37:32 +0530

    Update S3-test.ino

--- S3-test.ino
+++ S3-test.ino
@@ -1,54 +1,35 @@
-#include <Adafruit_NeoPixel.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <WiFi.h>
-#include <ArduinoJson.h>
-#include <RemoteXY.h>
-#include <ESP32Servo>
-#include <FastLED.h>
+#include <ESP32Servo.h>
 
-#define LED_PIN 48
-#define LED_COUNT 1
+Servo myServo;
 
-Adafruit_NeoPixel pixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-StaticJsonDocument<200> jsonDoc;
-Servo testServo;
+const int SERVO_PIN = 4;   // choose a PWM-capable pin
 
 void setup() {
-
   Serial.begin(115200);
+  Serial.println("ESP32-S3 Servo Test Started");
 
-  // NeoPixel
-  pixel.begin();
-  pixel.setBrightness(50);
-  pixel.show();
+  // Attach servo to pin
+  myServo.attach(SERVO_PIN);
 
-  // JSON
-  jsonDoc["status"] = "running";
-
-  // I2C + SPI
-  Wire.begin();
-  SPI.begin();
-
-  // Servo
-  testServo.attach(2);
-
-  // FastLED dummy init
-  FastLED.addLeds<NEOPIXEL, LED_PIN>((CRGB*)nullptr, 0);
-
-  Serial.println("Multi-library test sketch running!");
+  // Move to initial position
+  myServo.write(90);
+  delay(500);
 }
 
 void loop() {
-  pixel.setPixelColor(0, pixel.Color(255, 0, 0));
-  pixel.show();
-  delay(300);
+  Serial.println("Sweeping → 0 → 180");
+  
+  // Sweep forward
+  for (int pos = 0; pos <= 180; pos++) {
+    myServo.write(pos);
+    delay(10);
+  }
 
-  pixel.setPixelColor(0, pixel.Color(0, 255, 0));
-  pixel.show();
-  delay(300);
+  // Sweep backward
+  for (int pos = 180; pos >= 0; pos--) {
+    myServo.write(pos);
+    delay(10);
+  }
 
-  pixel.setPixelColor(0, pixel.Color(0, 0, 255));
-  pixel.show();
-  delay(300);
+  delay(500);
 }
