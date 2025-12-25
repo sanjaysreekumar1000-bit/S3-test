d81705f1422694d1dd5f5f3da1ac8981f0d6b61f
sanjaysreekumar1000-bit <sanjaysreekumar1000@gmail.com>
2025-12-25 12:49:01 +0530

    Update S3-test.ino

--- S3-test.ino
+++ S3-test.ino
@@ -1,7 +1,25 @@
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN 48
+#define LED_COUNT 1
+
+Adafruit_NeoPixel pixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Hello, ESP32-S3!");
+  pixel.begin();
+  pixel.setBrightness(50);
 }
+
 void loop() {
-  delay(10);
+  pixel.setPixelColor(0, pixel.Color(255, 0, 0)); // RED
+  pixel.show();
+  delay(400);
+
+  pixel.setPixelColor(0, pixel.Color(0, 255, 0)); // GREEN
+  pixel.show();
+  delay(400);
+
+  pixel.setPixelColor(0, pixel.Color(0, 0, 255)); // BLUE
+  pixel.show();
+  delay(400);
 }
