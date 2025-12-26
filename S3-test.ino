c75244e2b324e52e50212059be3184641893d730
Echo <sanjaysreekumar1000@gmail.com>
2025-12-26 22:32:31 +0530

    Update S3-test.ino

--- S3-test.ino
+++ S3-test.ino
@@ -1,35 +1,7 @@
-#include <ESP32Servo.h>
-
-Servo myServo;
-
-const int SERVO_PIN = 4;   // choose a PWM-capable pin
-
 void setup() {
-  Serial.begin(115200);
-  Serial.println("ESP32-S3 Servo Test Started");
-
-  // Attach servo to pin
-  myServo.attach(SERVO_PIN);
-
-  // Move to initial position
-  myServo.write(90);
-  delay(500);
+   Serial.begin(115200);
+   
 }
-
 void loop() {
-  Serial.println("Sweeping → 0 → 180");
-  
-  // Sweep forward
-  for (int pos = 0; pos <= 180; pos++) {
-    myServo.write(pos);
-    delay(10);
-  }
-
-  // Sweep backward
-  for (int pos = 180; pos >= 0; pos--) {
-    myServo.write(pos);
-    delay(10);
-  }
-
-  delay(500);
+    Serial.print("Hello World!");
 }
